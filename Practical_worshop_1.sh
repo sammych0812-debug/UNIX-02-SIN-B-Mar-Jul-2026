bc55adfbb396a65fb9971d0bb52f5058ac54b2cf
sammych0812-debug <arikarinfu.san0812@gmail.com>
2026-03-25 02:08:04 +0000

    Key generation

---
+++ Practical_worshop_1.sh
@@ -0,0 +1,5 @@
+uname -a # Da la información completa del sistema operativo  / respuesta: Linux codespaces-dd4d4b 6.8.0-1044-azure #50~22.04.1-Ubuntu SMP Wed Dec  3 15:13:22 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux
+which gpg # Nos dice en qué ruta está instalado GPG / respuesta: /usr/bin/gpg
+gpg --version # # Muestra la versión de GPG instalada / respuesta : gpg (GnuPG) 2.4.4 ....
+gpg --full-generate-key # generar clave
+gpg --list-keys # listar llaves 
